215c34c9970e010b99bd778de8467f0b376f1b0a
fergus-g <59649706+fergus-g@users.noreply.github.com>
2022-06-08 16:26:25 +0100

    Dropdown
    Reverted Changes and made it simpler.  Created a dropdown component

--- src/components/Campaign/CampaignSelector.js
+++ src/components/Campaign/CampaignSelector.js
@@ -1,7 +1,6 @@
 import { Fragment, useState } from 'react';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import Dropdown from 'react-bootstrap/Dropdown'
 import classes from './CampaignSelector.module.css'
+import Dropdown from '../UI/Dropdown';
 
 
 
@@ -20,24 +19,7 @@ const CampaignSelector = (props) => {
         <label>
       Please select what campaign you are running:
       </label>
-        <DropdownButton
-        title="Dropdown right"
-        id="dropdown-menu-align-right"
-        onSelect={dropdownHandler}
-        >
-              <Dropdown.Item eventKey="default"></Dropdown.Item>
-              <Dropdown.Item eventKey="icewind">Icewind Dale: Rime of the Frost Maiden</Dropdown.Item>
-              <Dropdown.Item eventKey="strahd">Curse of Strahd</Dropdown.Item>
-              <Dropdown.Item eventKey="waterdeep">Waterdeep: Dragon Heist</Dropdown.Item>
-      </DropdownButton>
-      {/* <div className={classes.dropdown}>
-        <select onSelect={dropdownHandler} className={classes.dropbtn}>
-          <option value="default"></option>
-          <option value="icewind">Icewind Dale: Rime of the Frost Maiden</option>
-          <option value="strahd">Curse of Strahd</option>
-          <option value="waterdeep">Waterdeep: Dragon Heist</option>
-        </select>  
-     </div>     */}
+    <Dropdown />
     </div>
     </section>
 </Fragment>
---
+++ src/components/UI/Dropdown.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import classes from './Dropdown.module.css'
+
+const Dropdown = () => {
+
+    const [campaign, setCampaign] = useState();
+
+    const dropdownHandler = (e) => {
+        console.log(e);
+        setCampaign(e);
+    }
+    return <div className={classes.dropdown}>
+      <select onSelect={dropdownHandler} className={classes.dropbtn}>
+        <option value="default"></option>
+        <option value="icewind">Icewind Dale: Rime of the Frost Maiden</option>
+        <option value="strahd">Curse of Strahd</option>
+        <option value="waterdeep">Waterdeep: Dragon Heist</option>
+      </select>  
+   </div>   
+};
+
+
+export default Dropdown;
