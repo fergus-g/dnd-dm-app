9fbbc01f31bd6612ef79088b3ff0bd00cd2f91d5
fergus-g <59649706+fergus-g@users.noreply.github.com>
2022-06-09 10:50:59 +0100

    Added Quests

--- src/components/UI/Dropdown.js
+++ src/components/UI/Dropdown.js
@@ -2,15 +2,10 @@ import { useState } from "react";
 import classes from "./Dropdown.module.css";
 
 const Dropdown = (props) => {
-  const [campaign, setCampaign] = useState();
 
-  const dropdownHandler = (e) => {
-    console.log(e);
-    setCampaign(e);
-  };
   return (
     <div className={classes.dropdown}>
-      <select onSelect={dropdownHandler} className={classes.dropbtn}>
+      <select onSelect={() => props.campaignId} className={classes.dropbtn}>
         <option value="campaign"></option>
         <option value="icewind">Icewind Dale: Rime of the Frost Maiden</option>
         <option value="strahd">Curse of Strahd</option>
