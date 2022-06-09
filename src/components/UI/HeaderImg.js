569a0126e31c8da32cb242938ead27055666c932
fergus-g <fergus.gildea@utrack.com>
2022-06-09 23:32:25 +0100

    Commit

--- src/components/UI/HeaderImg.js
+++ src/components/UI/HeaderImg.js
@@ -1,14 +1,14 @@
 import icewind from "../../assets/icewind-header.jpg";
 import strahd from "../../assets/curseofstrahd-header.jpg";
 import waterdeep from "../../assets/waterdeep-header.jpg";
-import classes from '../Layout/Header.module.css'
+import classes from "../Layout/Header.module.css";
 
-const HeaderImg = props => {
-    return(
-        <div className={classes["main-image"]}>
-        <img src={props.image} alt="campaign header" />
-      </div>
-    );
+const HeaderImg = (props) => {
+  return (
+    <div className={classes["main-image"]}>
+      <img src={props.image} alt="campaign header" />
+    </div>
+  );
 };
 
 export default HeaderImg;
